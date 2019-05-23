64a5352c55788002cdce1a3e7328e7e485580ce4
jean-smaug <maximeblanc.dev@gmail.com>
2019-05-23 23:22:44 +0200

    test: add snapshot testing on App.vue (#39)

---
+++ src/__tests__/App.spec.js
@@ -0,0 +1,43 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Vuex from "vuex";
+
+import App from "../App";
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const $route = {
+  params: "adz93dze823j"
+};
+
+describe("App.vue", () => {
+  let store;
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        user: {
+          namespaced: true,
+          getters: {
+            isAuth: () => true
+          }
+        },
+        app: {
+          namespaced: true,
+          state: { notifications: [] }
+        }
+      }
+    });
+  });
+  it("should render", () => {
+    const wrapper = shallowMount(App, {
+      localVue,
+      store,
+      mocks: {
+        $route
+      },
+      stubs: ["router-link", "router-view"]
+    });
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
