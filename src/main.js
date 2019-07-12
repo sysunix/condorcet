7cbf96f2a46365961672d26fc597762964d7cea7
jean-smaug <maximeblanc.dev@gmail.com>
2019-07-12 23:37:09 +0200

    feat: add sentry to track errors (#79)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueClipboard from "vue-clipboard2";
 
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
+
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
@@ -10,6 +13,11 @@ import "./assets/main.css";
 Vue.config.productionTip = false;
 
 Vue.use(VueClipboard);
+
+Sentry.init({
+  dsn: "https://cce0e1f68c3d4625b5e9507dc1546242@sentry.io/1503931",
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+});
 
 new Vue({
   store,
