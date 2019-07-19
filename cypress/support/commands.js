4d759e9d3298912d250427eb230c8e3d116599ff
jean-smaug <maximeblanc.dev@gmail.com>
2019-07-19 18:00:57 +0200

    chore: update nodejs version for functions (#101)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,7 +26,6 @@
 
 import firebase from "@firebase/app";
 import "@firebase/auth";
-import "@firebase/database";
 import "@firebase/firestore";
 import { attachCustomCommands } from "cypress-firebase";
 
