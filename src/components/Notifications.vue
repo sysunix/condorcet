293c5975f2868918721d88097549d104daaebf22
jean-smaug <maximeblanc.dev@gmail.com>
2019-08-10 17:37:34 +0200

    feat: improve loading and styles when joining poll (#126)

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -7,7 +7,7 @@
         :data-testid="`notification-${key}`"
         :value="true"
         :type="notification.status"
-        class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+        class="mt-2 bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
         role="alert"
       >
         <p>{{ notification.text }}</p>
